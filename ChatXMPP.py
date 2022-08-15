988624af42665f5481e8466e35ab865bd0072613
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2022-08-15 14:28:14 -0600

    mensajes, lista de contactos, añadir contactos

--- ChatXMPP.py
+++ ChatXMPP.py
@@ -28,68 +28,107 @@ class Cliente(slixmpp.ClientXMPP):
         slixmpp.ClientXMPP.__init__(self, usu, password)
         self.usu = usu
         self.password = password
-        self.add_event_handler("session_start", self.login)
-        #self.add_event_handler("register", self.registro)
+        self.add_event_handler("session_start", self.start)
+        #self.add_event_handler("add_contact", self.AddContact)
         #self.add_event_handler("message", self.mensaje)
 
-    async def login(self, event):
+    def DM(self):
+        para = input("Ingrese el contacto(ejemplo@alumchat.fun): ")
+        msg = input("Ingrese mensaje:  ")
+        self.send_message(mto = para, mbody = msg, mtype = "chat")
+    
+    def logout(self):
+        self.disconnect()
+        print("-----Sesión cerrada, Conectese Pronto:)-----")
+    
+    # 1 to 1 message
+    def sendMsg(self,to):
+        message = input("Ingrese mensaje a enviar: ")
+        self.send_message(mto=to,
+                          mbody=message,
+                          mtype='chat')
+
+    def AddContact(self):
+        contacto = input("Ingrese el contacto(ejemplo@alumchat.fun): ")
+        try:
+            self.send_presence_subscription(pto=contacto) 
+            self.send_message(mto=contacto,
+                          mbody="Hola, quieres ser mi amigo?",
+                          mtype='chat')
+        except IqTimeout:
+            print("Conexión Perdida") 
+        
         self.send_presence()
+        self.get_roster()
 
-        def DM():
-            to = input("Ingrese el contacto(ejemplo@alumchat.fun): ")
-            msg = input("Ingrese mensaje:  ")
-            self.send_message(mto = to, mbody = msg, mtype = "chat")
-            print("0/100%")
-            time.sleep(3)
-            print("---25/100%")
-            print("------50/100%")
-            time.sleep(3)
-            print("---------75/100%")
-            print("------------100/100%")
-            time.sleep(3)
-            print("-----Enviado Exitosamente-----")
-        
-        def logout():
-            self.disconnect()
-            print("-----Sesión cerrada, Conectese Pronto:)-----")
-        
-        def AddContact():
-            contacto = input("Ingrese usuario del contacto a agregar(ejemplo@alumchat.fun): ")
-            self.send_presence_subscription(pto = contacto)
-            print("0/100%")
-            time.sleep(3)
-            print("---33/100%")
-            print("------66/100%")
-            time.sleep(3)
-            print("----------100/100%")
-            print("-----Has añadido a " + str(contacto) + " exitosamente-----")
-        
-        def Delete():
-            self.send_presence()
-            self.get_roster()
+    def Delete(self):
+        self.send_presence()
+        self.get_roster()
 
-            delete = self.Iq()
-            delete['type'] = 'set'
-            delete['from'] = self.user
-            fragment = ET.fromstring("<query xmlns='jabber:iq:register'><remove/></query>")
-            delete.append(fragment)
+        delete = self.Iq()
+        delete['type'] = 'set'
+        delete['from'] = self.user
+        fragment = ET.fromstring("<query xmlns='jabber:iq:register'><remove/></query>")
+        delete.append(fragment)
 
-            try:
-                delete.send()
-                print("Cuenta Borrada")
-            except IqError as e:
-            
-                print("Error", e)
-            except IqTimeout:
-
-                print("timeout del server")
-            except Exception as e:
+        try:
+            delete.send()
+            print("Cuenta Borrada")
+        except IqError as e:
         
-                print(e)  
+            print("Error", e)
+        except IqTimeout:
 
-            self.disconnect()
-        
-        while True:
+            print("timeout del server")
+        except Exception as e:
+    
+            print(e)  
+
+        self.disconnect()   
+
+    def ShowContacts(self):
+        print('Contactos %s' % self.boundjid.bare)
+        groups = self.client_roster.groups()
+        for group in groups:
+            print('\n%s' % group)
+            print('-' * 72)
+            for jid in groups[group]:
+                name = self.client_roster[jid]['name']
+                if self.client_roster[jid]['name']:
+                    print(' %s (%s)' % (name, jid))
+                else:
+                    print('\n',jid)
+
+                connections = self.client_roster.presence(jid)
+                for res, pres in connections.items():
+                    show = 'available'
+                    if pres['show']:
+                        show = pres['show']
+                    print('   - %s (%s)' % (res, show))
+                    if pres['status']:
+                        print('       %s' % pres['status'])
+                        print('-' * 72)
+
+    def UserInfo(self):
+        self.get_roster()
+        usuario = input("Usuario: ")
+        contactos = self.client_roster.presence(usuario)
+        for res, pres in contactos.items():
+            show = 'chat'
+            if pres['show']:
+                show = pres['show']
+            print("     INFO:")
+            print('        ', show)
+            print('     Estado: ', pres['status'])
+
+    async def start(self, event):
+        self.send_presence()
+        await self.get_roster()
+
+        menu = True
+        logging.basicConfig(level=logging.DEBUG, format=None)
+
+        while (menu):
             print("-----------------------------")
             print("|     MENU DE FUNCIONES     |")
             print("-----------------------------")
@@ -98,14 +137,46 @@ class Cliente(slixmpp.ClientXMPP):
             print("|3.  Detalles Contacto      |")
             print("|4.  Mensaje Directo        |")
             print("|5.  Conversación Grupal    |")
-            print("|6.  Mensaje de Presenca    |")
-            print("|7.  Notificaciones         |")
-            print("|8.  Archivos               |")
-            print("|9.  Eliminar               |")
-            print("|10. Salir                  |")
+            print("|6.  Mensaje de Presencia   |")
+            print("|7.  Archivos               |")
+            print("|8.  Eliminar               |")
+            print("|9.  Salir                  |")
             print("-----------------------------")
             op = input("Ingrese opción:\t")
-  
+
+            if (op == "1"):
+                self.ShowContacts()
+            
+            elif (op == "2"):
+                self.AddContact()
+            
+            elif(op == "3"):
+                self.UserInfo()
+            
+            elif(op == "4"):
+                self.DM()
+                self.logout()
+            
+            elif(op == "5"):
+                print("pendiente")
+
+            elif(op == "6"):
+                print("pendiente")
+
+            elif(op == "7"):
+                print("pendiente")
+
+            elif(op == "8"):
+                print("pendiente")
+
+            elif(op == "9"):
+                menu = False
+                self.logout()
+            
+            self.send_presence()
+            await self.get_roster()
+
+logging.basicConfig(level=logging.DEBUG, format=None)
 op = ""
 print("----------------------------")
 print("|     MENU DE OPCIONES     |")
@@ -117,23 +188,18 @@ print("----------------------------")
 op = input("Ingrese opción:\t")
 
 if op == "1":
-    usu = input("Ingrese usuario(usuario@alumchat.fun): ")
-    password = getpass("Ingrese contraseña: ")
-    xmpp = Cliente(usu,password)
-    xmpp.register_plugin('xep_0004') # Data forms
-    xmpp.register_plugin('xep_0066') # Out-of-band Data
-    xmpp.register_plugin('xep_0077')
-    xmpp.register_plugin('xep_0085')
-    xmpp['xep_0077'].force_registration = True
-    xmpp.connect()
-    xmpp.process()
+    #Registro pendiente NO SIRVE
+    usu = input("Ingrese nuevo usuario: ")
+    psd = getpass("Ingrese contraseña: ")
 
 elif op == "2":
     usu = input("Ingrese usuario(usuario@alumchat.fun): ")
     password = getpass("Ingrese contraseña: ")
     xmpp = Cliente(usu, password)
     xmpp.register_plugin('xep_0030') 
-    xmpp.register_plugin('xep_0199') 
+    xmpp.register_plugin('xep_0199')
+    xmpp.register_plugin('xep_0045') # Mulit-User Chat (MUC)
+    xmpp.register_plugin('xep_0096') # Jabber Search 
     xmpp.connect()
     xmpp.process(forever=False)
                 

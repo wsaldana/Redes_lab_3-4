ce2e56e2607fdd34cee9a0f2190e715cc7e4c2a4
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2022-09-01 18:43:48 -0600

    Calculate shortest path from an specific node in Dijsktra

--- src/routing.py
+++ src/routing.py
@@ -1,4 +1,5 @@
 import abc
+import sys
 
 from .models import Node
 from .topology import Topology
@@ -88,8 +89,37 @@ class Dijkstra(Rounting):
     def route(self, sender: str, receiver: str) -> dict:
         ...
     
-    def Shortest_path(self):
-        ...
+    def Shortest_path(self, sender):
+        unvisited_nodes = list(self.nodes)
+ 
+        shortest_path = {}
+    
+        previous_nodes = {}
+      
+        max_value = sys.maxsize
+        for node in unvisited_nodes:
+            shortest_path[node] = max_value
+        
+        shortest_path[sender] = 0
+        
+        while unvisited_nodes:
+            current_min_node = None
+            for node in unvisited_nodes: 
+                if current_min_node == None:
+                    current_min_node = node
+                elif shortest_path[node] < shortest_path[current_min_node]:
+                    current_min_node = node
+                    
+            neighbors = self.get_outgoing_edges(current_min_node)
+            for neighbor in neighbors:
+                tentative_value = shortest_path[current_min_node] + self.value(current_min_node, neighbor)
+                if tentative_value < shortest_path[neighbor]:
+                    shortest_path[neighbor] = tentative_value
+                    previous_nodes[neighbor] = current_min_node
+    
+            unvisited_nodes.remove(current_min_node)
+        
+        return previous_nodes
 
 class Router:
 
